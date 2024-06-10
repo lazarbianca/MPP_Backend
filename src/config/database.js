caa7102e29a33ebdb85e7d03d69a2038846895a9
lazarbianca <161320992+lazarbianca@users.noreply.github.com>
2024-06-10 11:07:13 +0300

    db url

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,10 @@
 const { Sequelize } = require("sequelize");
 const crypto = require("crypto");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-  }
-);
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+});
 
 // const sequelize = new Sequelize("MPP_PAINTINGS", "postgres", "0000", {
 //   host: "localhost",
