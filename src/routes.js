32c1896f00cb3471205bfb43704ca943cc4b3519
lazarbianca <161320992+lazarbianca@users.noreply.github.com>
2025-04-08 08:31:49 +0300

    registration debug

--- src/routes.js
+++ src/routes.js
@@ -34,7 +34,7 @@ router.post(
       });
       res.status(201).json({ message: "User registered", user });
     } catch (err) {
-      console.log(err.message);
+      console.log(err);
       res.status(500).json({ error: err.message });
     }
   }
